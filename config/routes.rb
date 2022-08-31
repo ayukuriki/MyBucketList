48c2d403077ab6f6bcc207aeab556b326340eba7
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-08-31 18:49:22 +0900

    [add] create Congrat model

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   get "my_achieved_lists" => "achieved_lists#my_index"
   resources :lists, only: [:new, :create, :edit, :update, :destroy]
   resources :achieved_lists, only: [:index, :new, :create, :destroy]
+  resources :achieved_lists do
+    resource :congrats, only: [:create, :destroy]
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_093121) do
+ActiveRecord::Schema.define(version: 2022_08_31_094525) do
 
   create_table "achieved_lists", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2022_08_31_093121) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "congrats", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "achieved_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["achieved_list_id"], name: "index_congrats_on_achieved_list_id"
+    t.index ["user_id"], name: "index_congrats_on_user_id"
   end
 
   create_table "lists", charset: "utf8mb4", force: :cascade do |t|
@@ -54,4 +63,6 @@ ActiveRecord::Schema.define(version: 2022_08_31_093121) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "congrats", "achieved_lists"
+  add_foreign_key "congrats", "users"
 end
