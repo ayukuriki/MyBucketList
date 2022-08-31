e58596d91e4c44dde788982132047ea178fc796f
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-08-31 17:03:49 +0900

    [add] set routes for List

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'top#index'
+  get "mypage" => "lists#index"
+  get "sharepage" => "lists#all_index" 
+  resources :lists, only: [:new, :create, :edit, :update, :destroy]
 end
