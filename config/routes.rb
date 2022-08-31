740aff6f93d06efab2101d1c38a53263021c6b2f
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-08-31 19:18:17 +0900

    [add] create User controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "mypage" => "lists#index"
   get "sharepage" => "lists#all_index" 
   get "my_achieved_lists" => "achieved_lists#my_index"
+  get "users/show" => "users#show"
   resources :lists, only: [:new, :create, :edit, :update, :destroy]
   resources :achieved_lists, only: [:index, :new, :create, :destroy]
   resources :questions, only: [:index, :show, :new, :create]
