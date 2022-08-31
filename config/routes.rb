f1385a866cbbba85d72dead50f77fac8fa8e8f38
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-08-31 17:48:58 +0900

    [add] install devise and User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'top#index'
   get "mypage" => "lists#index"
   get "sharepage" => "lists#all_index" 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_074321) do
+ActiveRecord::Schema.define(version: 2022_08_31_082931) do
 
   create_table "lists", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
