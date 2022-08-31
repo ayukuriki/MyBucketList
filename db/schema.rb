17bf8dd9bb1f64d62c23630ca4473412002a6495
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-08-31 18:25:52 +0900

    [add] create relations among models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_31_082931) do
+ActiveRecord::Schema.define(version: 2022_08_31_090550) do
+
+  create_table "categories", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "lists", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
