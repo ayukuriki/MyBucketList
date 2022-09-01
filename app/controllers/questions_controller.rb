4350c445b26a4faee01b4d9399c6bc5ee424ab2d
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-09-01 14:35:38 +0900

    [add] define actions on Question controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,20 +1,40 @@
 class QuestionsController < ApplicationController
+  before_action :set_categories
 
   def index
-    
+    @questions = Question.all
   end
 
   def show
-  
+    @question = Question.find(params[:id])
+    @category = @categories.find(@question.category_id)
+    @user = User.find(@question.user_id)
+    @users = User.all
+    @advices = @question.advices
+    @advice = current_user.advices.new
   end
 
   def new
-
+    @question = Question.new
   end
 
   def create
-
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    if @question.save
+      redirect_to questions_path, notice: '新規質問が追加されました'
+    else
+      render "new"
+    end
   end
 
+  private
 
+  def question_params
+    params.require(:question).permit(:title, :content, :category_id, :user_id)
+  end
+
+  def set_categories
+    @categories = Category.all
+  end
 end
