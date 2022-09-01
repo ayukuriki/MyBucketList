78d318b36819616df5046098b3e22e40122cc97d
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-09-01 14:57:12 +0900

    [add] define actions on User controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
-    
+    @user = current_user
+    @users = User.all
+    @lists = List.all.where(user_id: current_user.id)
+    @achieved_lists = AchievedList.all.where(user_id: current_user.id)
   end
 end
