e3a62752c8560b3a0131d75f4f514c7447614696
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-09-01 14:54:30 +0900

    [add] define actions on Advice controller

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -1,10 +1,27 @@
 class AdvicesController < ApplicationController
   def create
-
+    @advice = current_user.advices.new(advice_params)
+    if @advice.save
+      flash[:success] = "アドバイスを投稿しました"
+      redirect_to question_path(@advice.question_id)
+    else
+      render question_path(@advice.question_id)
+    end
   end
 
   def destroy
-
+    @advice = Advice.find(params[:id])
+    if current_user.id == @advice.user.id
+      @advice.destroy!
+      redirect_to question_path(@advice.question_id)
+    else
+      render question_path(@advice.question_id)
+    end
   end
 
+  private
+
+  def advice_params
+    params.require(:advice).permit(:content, :question_id, :user_id)
+  end
 end
