690c7760b4065b770039567ec54c65adb8be2ae3
栗城愛由 <kurikiaiyu@kurikiaiyunoMacBook-Air-2.local>
2022-10-06 12:49:07 +0900

    [fix] solve rspec error

--- spec/requests/questions_spec.rb
+++ spec/requests/questions_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "questions", type: :request do
   let!(:question1) { create(:question, title: "質問タイトルです", user_id: current_user.id) }
   let!(:question2) { create(:question, title: "質問2タイトル", user_id: user1.id) }
   let!(:category1) { create(:category, id: 1) }
-  let!(:advice) { create(:advice, question_id: question1.id, user_id: user2.id) }
+  let!(:advice) { create(:advice, question_id: question1.id) }
   let(:current_user) { create(:user, name: "ログインユーザー", email: "login@gmail.com") }
   let(:user1) { create(:user, name: "ユーザー1", email: "user1@gmail.com") }
   let(:user2) { create(:user, name: "ユーザー2", email: "user2@gmail.com") }
@@ -27,6 +27,7 @@ RSpec.describe "questions", type: :request do
 
   describe "質問詳細ページの表示" do
     before do
+      sign_in current_user
       get question_path(question1.id)
     end
 
